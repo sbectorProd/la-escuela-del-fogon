3016937ce5c0f008f1118f00c6cce90b2d4b2cb0
No_otra_vez <saulbector@gmail.com>
2023-11-24 03:46:26 -0600

    Adopting tailwind's mobil first

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='bg-amber-50'>
+    <html lang="en" className='bg-amber-50 dark:bg-slate-500'>
       <body className={degheest.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col flex-wrap items-center justify-center">
-      <div className="max-w-lg">
-        <h1 className="text-7xl text-black text-justify leading-tight">LA ESCUELA DEL FOGÓN
-          <span className="w-full inline-block leading-4"></span></h1>
-        <h2 className="text-lg text-center">COCINANDO LA REVOLUCIÓN EN AMÉRICA</h2>
+      <div className="w-[300px] sm:w-[500px]">
+        <h1 className="text-[42px] sm:text-[70px] text-stone-700 dark:text-amber-50 text-justify leading-tight">LA ESCUELA DEL FOGÓN
+          <span className="w-full inline-block"></span></h1>
       </div>
+        <Link href={'./'} className="pb-20">
+          <div className="bg-pink-400 shadow-xl py-2 w-[300px] sm:w-[500px] shadow-amber-800 ">
+            <h2 className="text-[10px] sm:text-[16px] text-slate-700 text-center">COCINANDO LA REVOLUCIÓN EN AMÉRICA</h2>
+          </div>
+        </Link>
     </main>
   )
 }
+
+const className = ''
