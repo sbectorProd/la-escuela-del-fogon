78cd151aed6f0afc5020ecbabf352fbdaa816a6a
No_otra_vez <saulbector@gmail.com>
2023-11-29 18:38:52 -0600

    Hover

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button( {children, onClick}:{children:React.ReactNode, onClick?:any} ) {
     return (
-        <button onClick={onClick} className="bg-gray-800 text-white py-2 px-3 rounded-xl m-6">
+        <button onClick={onClick} className="bg-gray-800 text-white hover:bg-white hover:text-black py-2 px-3 rounded-xl m-6">
             {children}
         </button>
     )
