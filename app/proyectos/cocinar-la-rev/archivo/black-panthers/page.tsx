883c4e493db7998ee86a015b738aa5c61e62377a
sbectorProd <sbector.prod@gmail.com>
2024-10-21 20:20:51 -0600

    Add black panthers page

---
+++ app/proyectos/cocinar-la-rev/archivo/black-panthers/page.tsx
@@ -0,0 +1,8 @@
+export default function BlackPanthersPage() {
+    return(
+        <div>
+            <h1>Hola!</h1>
+            <p>Esta es la página de black panthers</p>
+        </div>
+    )
+}
